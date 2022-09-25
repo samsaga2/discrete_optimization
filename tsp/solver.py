edb55636f37bf4ac37c5654d2b9a9c70358183b3
Víctor M <samsaga2@gmail.com>
2022-09-25 10:06:13 +0200

    I'm fucked

--- tsp/solver.py
+++ tsp/solver.py
@@ -1,97 +1,99 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import array
+import os
+import array
+import sys
 import math
 import random
-import functools
 from collections import namedtuple
 
 Point = namedtuple("Point", ['x', 'y'])
 
-@functools.cache
+
 def length(point1, point2):
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
 
-def calc_length(solution, points):
+
+def total_length(solution, points):
     nodeCount = len(points)
     obj = length(points[solution[-1]], points[solution[0]])
-    for index in range(0, nodeCount-1):
+    for index in range(nodeCount-1):
         obj += length(points[solution[index]], points[solution[index+1]])
     return obj
 
-def random_swap(nodes, points):
-    i = random.randrange(0, len(nodes) - 1)
-    # while True:
-    #     j = random.randrange(0, len(nodes))
-    #     if j != i:
-    #         break
 
-    # i = random.randrange(0, len(best_nodes))
-    j = random.randrange(i+1, len(nodes))
-
-    # choices = range(i+1, len(nodes))
-    # weights = [length(points[nodes[i]], points[nodes[j]]) for j in choices]
-    # j = random.choices(choices, weights=weights)[0]
-
-    new_nodes = list(nodes)
-    new_nodes[i], new_nodes[j] = nodes[j], nodes[i]
-
-    return new_nodes
-
-def solve(points):
-    nodeCount = len(points)
-    best_nodes = list(range(0, nodeCount))
-    best_cost = calc_length(best_nodes, points)
-
-    max_depth = 5000
-
-    n = 0
+def find_nearest(node1, nodes, points):
+    costs = [1.0/(1+length(points[nodes[node1]], points[nodes[i]]))
+             for i in range(len(nodes))]
+    costs[node1] = 0
     while True:
-        new_nodes = list(best_nodes)
-        for k in range(0,10):
-            i = random.randrange(0, len(new_nodes))
-            j = random.randrange(0, len(new_nodes))
-            new_nodes[i], new_nodes[j] = new_nodes[j], new_nodes[i]
-
-            new_cost = calc_length(new_nodes, points)
-            if new_cost < best_cost:
-                best_cost = new_cost
-                best_nodes = new_nodes
-                # print(best_cost)
-                n=0
-                break
-        new_nodes = list(best_nodes)
-        n+=1
-        if n==int(max_depth//3) or n==int(max_depth//3)*2:
-            # print("split")
-            l = len(new_nodes)//3
-            i = random.randrange(0, len(new_nodes)-l)
-            j = random.randrange(0, l)
-            split=new_nodes[i:i+j]
-            del new_nodes[i:i+j]
-            split=split+new_nodes
-            new_nodes=split
-            new_cost = calc_length(new_nodes, points)
-            if new_cost < best_cost:
-                best_cost = new_cost
-                best_nodes = new_nodes
-                # print(best_cost)
-                n=0
-        if n>max_depth:
+        n = random.choices(nodes, weights=costs)[0]
+        #print(n)
+        if node1 != n:
             break
+    # n = min(range(len(points)), key=costs.__getitem__)
+    # print(node1, n)
+    return n
+
+
+def solve1(nodes, points, title):
+    nodeCount = len(points)
+    cost = total_length(nodes, points)
+
+    max_tries = 10 if nodeCount < 1000 else 1
+
+    tries = 0
+    while tries < max_tries:
+        for node in range(len(nodes)):
+            node1 = node
+            node2 = find_nearest(node1, nodes, points)
+
+            new_nodes = list(nodes)
+            new_nodes[node1], new_nodes[node2] = new_nodes[node2], new_nodes[node1]
+
+            new_cost = total_length(new_nodes, points)
+            #print(node, len(nodes))
+
+            # if node%10==0:
+            #     print("{} tries={} node->{}/{} ({}%)\r".format(title, tries, node, nodeCount, 100*node//len(nodes)), end="", file=sys.stderr)
+
+            if new_cost < cost:
+                nodes = new_nodes
+                cost = new_cost
+                tries = 0
+        tries += 1
+        #os.system('title '+title+' cost='+str(cost)+' tries='+str(tries))
+
+    return cost, nodes
+
+
+def solve(points, title):
+    nodeCount = len(points)
+    nodes = list(range(0, len(points)))
+    random.shuffle(nodes)
+    best_cost, best_nodes = solve1(nodes, points, title)
+
+    if len(points) < 1000:
+        max_tries = 100
+        for i in range(max_tries):
+            # random.shuffle(nodes)
+            cost, nodes = solve1(nodes, points, title +
+                                ' best_cost=' + str(best_cost) + ' solver_iter='+str(i))
+            if cost < best_cost:
+                best_cost = cost
+                best_nodes = nodes
+                # print(cost)
+
+            if i % 20 == 0:
+                for k in range(10):
+                    node1 = random.randrange(0, nodeCount)
+                    node2 = random.randrange(node1, node1+nodeCount) % nodeCount
+                    nodes[node1], nodes[node2] = nodes[node2], nodes[node1]
 
     return best_cost, best_nodes
 
-
-def solve_multiple(points):
-    best_best_cost, best_best_nodes = solve(points)
-    for tries in range(0, 5):
-        best_cost, best_nodes = solve(points)
-        if best_cost < best_best_cost:
-            best_best_cost = best_cost
-            best_best_nodes = best_nodes
-            # print(best_best_cost)
-    return best_best_cost, best_best_nodes
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -109,7 +111,21 @@ def solve_it(input_data):
 
     # build a trivial solution
     # visit the nodes in the order they appear in the file
-    obj, solution = solve_multiple(points)
+    obj, solution = solve(points, 'start')
+
+    if len(points) < 500:
+        max_tries = 10
+    elif len(points) < 1000:
+        max_tries = 5
+    else:
+        max_tries = 1
+
+    for i in range(max_tries):
+        title = 'best_tries={} best_solution={}'.format(i, obj)
+        obj1, solution1 = solve(points, title)
+        if obj1 < obj:
+            obj = obj1
+            solution = solution1
 
     # prepare the solution in the specified output format
     output_data = '%.2f' % obj + ' ' + str(0) + '\n'
@@ -117,8 +133,6 @@ def solve_it(input_data):
 
     return output_data
 
-
-import sys
 
 if __name__ == '__main__':
     import sys
@@ -129,4 +143,3 @@ if __name__ == '__main__':
         print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
-
